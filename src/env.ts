9ce3b703b068fc704d2c31405227eeb0de875cd0
Mykolas Slezevicius <mykolassle@gmail.com>
2026-03-23 00:52:08 +0100

    Make ANTHROPIC_API_KEY optional in env schema

--- src/env.ts
+++ src/env.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 const envSchema = z.object({
   GOOGLE_GENERATIVE_AI_API_KEY: z.string(),
-  ANTHROPIC_API_KEY: z.string(),
+  ANTHROPIC_API_KEY: z.string().optional(),
   APPIUM_URL: z.url().optional().default("http://127.0.0.1:4723"),
 });
 
